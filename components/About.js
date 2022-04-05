33395d95954a2346646aae191d72d40c92fa519d
M. Kepler <mmkepler@yahoo.com>
2022-04-05 12:16:05 -0600

    feat: section title

--- components/About.js
+++ components/About.js
@@ -1,12 +1,24 @@
 import sectionStyles from "../styles/Section.module.css";
+import melissa from "../public/images/me.png";
+import Image from "next/image";
 
 export default function About(){
   return (
-    <div className={sectionStyles.section} id={sectionStyles.about}>
-      <h2>About</h2>
+    <div>
+      <div className={sectionStyles.holder}>
+      <span className={sectionStyles.spanleft}></span><h2 className={sectionStyles.title}>About</h2><span className={sectionStyles.spanright}></span>
+      </div>
+    <div className={sectionStyles.section} 
+      id={sectionStyles.about} 
+      name="about">
+        <div id={sectionStyles.spacer}>
+        <Image src={melissa} alt="photo of Melissa Kepler photoshopped to look like it came from a space ship information display" width="100px" height="100px"/>
+        <p>Greetings!</p>
+        </div>
       <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
+    </div>
     </div>
   )
 }
--- components/Header.js
+++ components/Header.js
@@ -10,10 +10,10 @@ export default function Header(props){
     <nav id={headerStyles.nav}>
       <ul id={headerStyles.header}>
         <li><Link className={headerStyles.item} activeClass="active" to="home" spy={true} smooth={true}><Image alt="mmkepler.com logo" src={logo} width="60x" height="50px" ></Image></Link></li>
-        <li className={headerStyles.items}><Link to="about" spy={true} smooth={true}>About</Link></li>
-        <li className={headerStyles.items}><Link to="skills" spy={true} smooth={true}>Skills</Link></li>
-        <li className={headerStyles.items}><Link to="projects"  spy={true} smooth={true}>Projects</Link></li>
-        <li className={headerStyles.items}><Link to="contact"  spy={true} smooth={true}>Contact</Link></li>
+        <li className={headerStyles.items}><Link to="about" spy={true} smooth={true} offset={-100}>About</Link></li>
+        <li className={headerStyles.items}><Link to="skills" spy={true} smooth={true} offset={-100}>Skills</Link></li>
+        <li className={headerStyles.items}><Link to="projects"  spy={true} smooth={true} offset={-100}>Projects</Link></li>
+        <li className={headerStyles.items}><Link to="contact"  spy={true} smooth={true} offset={-100}>Contact</Link></li>
         <li id={headerStyles.toggle}>
           <button id={headerStyles.item} onClick={props.onClick}>
           <span className={headerStyles.hamburger}></span>
