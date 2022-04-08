7fbd87bbe06a898f4db7e4e4308fa9d19a414ec5
M. Kepler <mmkepler@yahoo.com>
2022-04-08 11:18:11 -0600

    feat: basic skills

--- components/Contact.js
+++ components/Contact.js
@@ -10,11 +10,15 @@ export default function Contact(){
       id={sectionStyles.contact} 
       name="contact">
         <div id={sectionStyles.spacer}>
-        <p>Greetings!</p>
+        <p>Send a message to my planet!</p>
         </div>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+      <div>
+      <form>
+        <input />
+        <inuputs />
+        <textarea></textarea>
+      </form>
+      </div>
     </div>
     </div>
   )
--- components/Skills.js
+++ components/Skills.js
@@ -1,12 +1,57 @@
 import sectionStyles from "../styles/Section.module.css";
+import Image from "next/image";
+import bullet from "../public/images/bullet.png";
 
 export default function Skills(){
   return (
-    <div className={sectionStyles.section} id={sectionStyles.skills} name="skills">
-      <h2>Skills</h2>
-      <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+    <div>
+      <div className={sectionStyles.holder}>
+      <span className={sectionStyles.spanleft}></span><h2 className={sectionStyles.title}>Skills</h2><span className={sectionStyles.spanright}></span>
+      </div>
+      <div className={sectionStyles.section} 
+        id={sectionStyles.skills} 
+        name="skills">
+      <div id={sectionStyles.skillsContainer}>
+        <ul>
+          <li>JavaScript/ES6+</li>
+          <li>React/Redux</li>
+          <li>CSS/FlexBox</li>
+          <li>Bootstrap</li>
+          <li>npm</li>
+          <li>Git/GitHub</li>
+          <li>JavaScript/ES6+</li>
+          <li>Node.js</li>
+          <li>Express</li>
+          <li>MongoDB</li>
+          <li>JQuery</li>
+          <li>Webpack/babel</li>
+          <li>FirebaseDB/Auth</li>
+          <li>Gulp/Grunt</li>
+          <li>Heroku/AWS</li>
+          <li>Cpanel</li>
+        </ul>
+        <ul>
+          <li><h3>Artistic</h3></li>
+          <li>Graphic Design</li>
+          <li>Photo Editing</li>
+          <li>Photoshop</li>
+          <li>Illustrator</li>
+          <li>Sketch</li>
+          <li>3D Animation/Modeling</li>
+        </ul>
+        <ul>
+          <li><h3>Working Knowledge:</h3></li>
+          <li>Next.js</li>
+          <li>Workbox/Service Workers/PWA</li>
+          <li>Google Analytics</li>
+          <li>Apollo/GraphQL</li>
+          <li>Python/PHP</li>
+          <li>Wordpress</li>
+          <li>SEO practices</li>
+        </ul>
+
+      </div>
+    </div>
     </div>
   )
 }
