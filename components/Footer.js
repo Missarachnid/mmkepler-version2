4782f6a2393c31745e0799e51d2b224d3d64d44d
M. Kepler <mmkepler@yahoo.com>
2022-04-18 15:58:27 -0600

    fix: header style issues

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,16 @@
 import footerStyles from "../styles/Footer.module.css";
+import twitter from "../public/images/Twitter_Social_Icon_Circle_White.png";
+import github from "../public/images/GitHub-Mark-Light-32px.png";
+import Image from "next/image";
 
 export default function Footer(){
   return (
     <footer id={footerStyles.footer}>
-      ©2022 Melissa Kepler
+      <p id={footerStyles.copy}>©2022 Melissa Kepler</p>
+      <div id={footerStyles.imageWrapper}>
+        <Image className={footerStyles.image} src={twitter} alt="Twitter logo" width={32} height={32}/>
+        <Image classame={footerStyles.image} src={github} alt="The GitHub Octocat logo" width={32} height={32} />
+      </div>
     </footer>
   );
 }
