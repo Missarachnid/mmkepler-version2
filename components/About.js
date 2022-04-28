db5fefd7b1430273a8fce4b6703b9bc9c39832a1
M. Kepler <mmkepler@yahoo.com>
2022-04-28 11:26:07 -0600

    feat: finished about section with added resume

--- components/About.js
+++ components/About.js
@@ -5,6 +5,7 @@ import linkedin from "../public/images/In-Black-34px-TM.png";
 import github from "../public/images/GitHub-Mark-32px.png";
 import Image from "next/image";
 import behance from "../public/images/behance.svg";
+import resume from "../public/images/resume.png";
 
 export default function About(){
   return (
@@ -21,18 +22,41 @@ export default function About(){
         <div id={aboutStyles.picWrapper}>
           <Image src={melissa} alt="photo of Melissa Kepler photoshopped to look like it came from a space ship information display" width="100px" height="100px"/>
           </div>
-          <h2>Greetings!</h2>
+          <h2>Greetings Earthlings!</h2>
         
         <div id={aboutStyles.textWrapper}>
           <p>
           I was the owner of Melissa Kepler Web Design &amp; Development, but I am currently looking for a full-time Front End or Full Stack React remote position.
           </p>
           <p>
-          I have been using every cheap or free resource I can to learn to be a JavaScript developer since 2015. I can’t remember what “Learn How To Code” website got me, but they got me. I love the idea of a career where you can grow through learning in ways that aren’t just traditional education.
+          I have been using every cheap or free resource I can to learn to be a JavaScript developer since 2015.
+           I can&apos;t remember what “Learn How To Code” website got me, but they got me.
+            I love the idea of a career where you can grow through learning in ways that aren&apos;t just traditional education.
           </p>
-          <p>I have finished the Front End tracks for Team Treehouse and Free Code Camp a while back. I have been working on the updated version of FreeCodeCamp&apos;s curriculum recently. In 2018 I won a Grow with Google scholarship for Udacity&apos;s Mobile Web Specialist Nanodegree and earned my Nanodegree. A lot of my learning has been through project creation. <a href="https://github.com/Missarachnid" id={aboutStyles.gitLink}>My GitHub is full of projects, please take a look!</a></p>
-          <p>Paragraph about Gap</p>
-          <p>I love space, and that might have something to do with my last name! I also love dinosaurs and wanted to be a paleontologist as a child (my favorite is Archaeopteryx!). In my spare time I am into bodybuilding, playing with my dogs, and reading and watching anything Carl Sagan ever worked on. I am a former member of Women Who Code Atlanta and I was a co-host of Woodstock Northwest OTP meetups before I relocated to Denver.</p>
+          <p>I earned Free Code Camp Certifications in Responsive Web Design, JavaScript Algorithms &#38; Data Structures, and Back End Development &#38; APIs.
+             In 2018 I won a Grow with Google scholarship and earned Udacity&apos;s Mobile Web Specialist Nanodegree.
+              A lot of my learning has been through project creation. &nbsp;
+              <a href="https://github.com/Missarachnid" id={aboutStyles.gitLink}>My GitHub is full of projects, please take a look!</a>
+          </p>
+          <p><strong>The dreaded resume gap!</strong> - Just as I was going to make my career jump into programming, my finance died from injuries caused by a car accident on our way to get married.
+             I had to settle his estate while dealing with my PTSD, and I took time off to do this. During this time I started my own business and did some graphic and web work. I also worked on studying as well.
+              In early 2020 I decided to move to Denver and go back to work. Then the country shut down, and I started having a lot of health issues. I didn&apos;t think I
+            had COVID, but I got a lot of symptoms of &lsquo;Long COVID&lsquo;. My body decided to go haywire. I fought it with all my energy and I am now feeling like I did before this nightmare began.
+             I am ready to take on the world again.
+          </p>
+          <p>I love space. That might have something to do with my last name!
+             I also love dinosaurs and wanted to be a paleontologist as a child (my favorite is Archaeopteryx!).
+              I am a former member of Women Who Code Atlanta and I was a co-host of Woodstock Northwest OTP meetups before I relocated to Denver.
+          </p>
+          <p>Although I included links, I don&apos;t spend much time on social media. Frankly I would rather just have tweets from birds.</p>
+        </div>
+        <div id={aboutStyles.resume}>
+          <a href="/images/Melissa_Kepler_resume.pdf">
+          <p>Check out my resume here : &nbsp; 
+            <Image src={resume} height={32} width={32}
+            className={aboutStyles.logos} alt="icon of a resume"/> 
+          </p>
+          </a>
         </div>
       </div>
 
@@ -48,7 +72,6 @@ export default function About(){
             </a>
           </div>
         
-
         <div className={aboutStyles.profSpacer}>
           <a className={aboutStyles.iconLink} href="https://github.com/Missarachnid">
             Coding Projects: 
@@ -61,8 +84,8 @@ export default function About(){
         <div className={aboutStyles.profSpacer}>
           <a className={aboutStyles.iconLink} href="https://www.behance.net/mmkepler">
             Artistic Work: 
-            <div className={aboutStyles.logos}>
-              <Image src={behance} alt="The Behance logo." width={32} height={32}/>
+            <div id={aboutStyles.behance}>
+              <Image src={behance} alt="The Behance logo." height={32} width={32}/>
             </div>
           </a>
         </div>
