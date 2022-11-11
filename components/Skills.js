ff482ce32f78db68dfc7ea9a14072f85a88f2c35
M. Kepler <mmkepler@yahoo.com>
2022-11-10 18:58:19 -0700

    feat: updated skill list and code stucture. Used Mapping instead of typing out

--- components/Skills.js
+++ components/Skills.js
@@ -2,6 +2,89 @@ import sectionStyles from "../styles/Section.module.css";
 import Image from "next/image";
 import planet from "../public/images/bullet.png";
 
+const codingSkills = ["JavaScript/ES6+", "React/Hooks", "Redux", "CSS/Flexbox", "Styled Components", "Bootstrap", "Sass", "Node.js", "Express.js", "MongoDB", "Accessibility", "npm", "Git/GitHub", "Firebase DB & Auth", "Webpack/Babel", "Gulp/Grunt", "Rest APIs", "JQuery"];
+const deploymentSkills = ["Cpanel", "Heroku", "AWS/S3", "Digital Ocean", "Netlify"];
+const knowledgeableSkills = ["Next.js", "Workbox/PWA", "Google Analytics", "Apollo/GraphQL", "Keystone.js", "Python/PHP", "WordPress", "SEO", "Cloudflare", "Adobe XD"];
+const artisticSkills = ["Graphic Design", "Photo Editing", "Photoshop", "Illustrator", "Sketch", "3D Animation/Modeling"];
+const allSkills = ["codingSkills", "deploymentSkills", "knowledgeableSkills", "artisticSkills"];
+
+export default function Skills(){
+  return (
+    <section id={sectionStyles.skills} 
+    name="skills">
+      <div className={sectionStyles.holder}>
+      <span className={sectionStyles.spanleft}></span><h2 className={sectionStyles.title} tabIndex="0">Skills</h2><span className={sectionStyles.spanright}></span>
+      </div>
+      <div className={sectionStyles.section} >
+      <div id={sectionStyles.skillsContainer}>
+        <div id={sectionStyles.skillsWrapper} >
+        
+        <div id={sectionStyles.columns}>
+        
+          <div id={sectionStyles.one}>
+            <div>
+            <h3 className={sectionStyles.skilltTitles}>Coding Skills</h3>
+            <ul>
+              {codingSkills.map(function(el, index){
+                return(
+                  <li tabIndex="0" key={index}><Image src={planet} alt="" width={10} height={9}/>{el}</li>
+                )
+              })}
+            </ul>
+          </div>
+
+        
+              
+          <div>
+            <h3 className={sectionStyles.skilltTitles}>Deployment Platforms</h3>
+            <ul>
+              {deploymentSkills.map(function(el, index){
+                return(
+                  <li tabIndex="0" key={index}><Image src={planet} alt="" width={10} height={9}/>{el}</li>
+                )
+              })}
+            </ul>
+          </div>
+
+          </div>
+
+          <div id={sectionStyles.two}>
+
+          <div>
+            <h3 className={sectionStyles.skilltTitles}>Knowledgeable In:</h3>
+            <ul>
+              {knowledgeableSkills.map(function(el, index){
+                return(
+                  <li tabIndex="0" key={index}><Image src={planet} alt="" width={10} height={9}/>{el}</li>
+                )
+              })}
+            </ul>
+          </div>
+
+          <div>
+            <h3 className={sectionStyles.skilltTitles}>Artistic Skills:</h3>
+            <ul>
+              {artisticSkills.map(function(el, index){
+                return(
+                  <li tabIndex="0" key={index}><Image src={planet} alt="" width={10} height={9}/>{el}</li>
+                )
+              })}
+            </ul>
+          </div>
+
+          </div>
+
+        </div>
+
+        </div>
+      </div>
+    </div>
+    </section>
+  )
+}
+
+
+/*
 export default function Skills(){
   return (
     <section id={sectionStyles.skills} 
@@ -57,3 +140,4 @@ export default function Skills(){
     </section>
   )
 }
+*/
